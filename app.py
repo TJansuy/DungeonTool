3318e096626b54d59d036cacd8b0c45d0ca9642e
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-05-18 03:12:19 -0700

    Adding functionality to dynamically get images based on input

--- app.py
+++ app.py
@@ -28,3 +28,20 @@ def delayed_response():
     print("Timer done!")
 
     return send_file("test_image.png", mimetype="image/png")
+
+@app.route("/submit")
+def submit_test():
+    import base64
+    print(request.query_string.decode("ascii"))
+    postHash = base64.urlsafe_b64encode(bytes(request.query_string.decode("ascii"), "utf_8"))
+    stringHash = postHash.decode("utf_8")
+    print("Checking for", stringHash)
+    # Check if File Exists
+    import os
+    if os.path.isfile(stringHash + ".png"):
+        print(stringHash, "found")
+        return send_file(stringHash + ".png", mimetype="image/png")
+    else:
+        print("No file exists for", stringHash)
+        # Return an image
+        return send_file("test_image.png", mimetype="image/png")
